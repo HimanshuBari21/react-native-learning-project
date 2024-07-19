ac14df4082ffde224ec0a5999fcedfe1295df784
Himanshu Bari <himanshubari789@gmail.com>
2024-07-20 02:24:47 +0530

    Integrated React Native Paper Lib

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,28 @@
-import { Text, View } from "react-native";
+import { useState } from "react";
+import { View, StyleSheet, Platform, PlatformColor } from "react-native";
+import { Appbar, Button, Text } from "react-native-paper";
 
-export default function Index() {
+export default function Home() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Appbar.Header>
+        <Appbar.BackAction onPress={() => {}} />
+        <Appbar.Content title="Title" />
+        <Appbar.Action icon="calendar" onPress={() => {}} />
+        <Appbar.Action icon="magnify" onPress={() => {}} />
+      </Appbar.Header>
+      <Button onPress={() => console.log(Platform.constants)}>
+        {Platform.OS.toUpperCase() +
+          " version " +
+          (Platform.Version ?? " Not Avalaible")}
+      </Button>
+      <Text>हिमांशु</Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+  },
+});
